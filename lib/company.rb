2ec148c21beae6e3eb7b56c52819d56e05d7d4e6
Monica Wilkinson <monica.keller@gmail.com>
2011-09-07 09:28:09 -0700

    Housekeeping

--- lib/company.rb
+++ lib/company.rb
@@ -1,6 +1,7 @@
 COMPANY_FIELDS = ['id', 'name', 'description', 'founded-year', 'universal-name', 'locations', 'email-domains', 'company-type', 'website-url', 'ticker', 'logo-url', 'twitter-id', 'employee-count_range']
 
 get '/companies' do
+  @title = 'Companies'
   @controller = 'companies'
   @data = lnk_show_all_companies
   haml :show_all
@@ -30,6 +31,7 @@ get '/company/:company_id' do |company_id|
   begin
     @company = lnk_client.company({:id => company_id, :fields => COMPANY_FIELDS})
     @@redis.set "company_#{company_id}", @company.to_json
+    @title = @company.name
   rescue OAuth2::Error => e
      return e.response.inspect
   end
@@ -43,7 +45,7 @@ end
 get  '/company/add/:company_id.json' do |company_id|
   if @@redis.get "company_#{company_id}"
     @@redis.sadd cart_id, "company_#{company_id}"
-    output = "Added #{company_id}"
+    redirect "/"
   end
 end
 

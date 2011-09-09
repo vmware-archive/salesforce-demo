0afb50b07f647597a658cf9214a0f089def51ed1
Monica Wilkinson <monica.keller@gmail.com>
2011-09-08 17:46:36 -0700

    Added the title for the record

--- lib/chatter.rb
+++ lib/chatter.rb
@@ -6,7 +6,7 @@ get '/chatter/me' do
   begin
     response = access_token.get("#{INSTANCE_URL}/services/data/v22.0/chatter/users/me", :headers => {'Content-type' => 'application/json'})
     @item_data = response.parsed
-    @title = @item_data['name']
+    @record_title = @item_data['name']
 
   rescue OAuth2::Error => e
      puts "Got error getting chatter current user #{e.response.inspect }"

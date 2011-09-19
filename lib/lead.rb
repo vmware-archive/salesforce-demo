3ba1ea47985bbd49080762a2bddc7093ec816305
Monica Wilkinson <monica.keller@gmail.com>
2011-09-19 14:50:34 -0700

    More info from LinkedIn Company to build the fake lead

--- lib/lead.rb
+++ lib/lead.rb
@@ -35,9 +35,27 @@ get '/leads/create' do
       domains = company['email_domains']['all']
     rescue
     end
+    contact_info = {}
+    address = {}
+    if (company['locations'] && company['locations']['all'] && company['locations']['all'].count > 0)
+      contact_info = company['locations']['all'][0]['contact_info']
+      address = company['locations']['all'][0]['address']
+    end
     gen = PersonGenerator.new (domains)
     person = gen.next_person
-    lead = {'FirstName' => person.first_name, 'LastName' =>person.last_name, 'Email' => person.email, 'Company' => "#{company['name']}"}
+    lead = {
+        'FirstName' => person.first_name,
+        'LastName' =>person.last_name,
+        'Email' => person.email,
+        'Company' => company['name'],
+        'Website' => company['website_url'],
+        'Industry' => company['industry'],
+        'Phone' => contact_info['phone1'],
+        'Street' => address['street1'],
+        'City' => address['city'],
+        'State' => address['region'],
+        'PostalCode' => address['postal_code']
+    }
     id = create 'lead', lead.to_json
     @messages << "Created lead <a href='/lead/#{id}'>#{id}</a>"
   end
